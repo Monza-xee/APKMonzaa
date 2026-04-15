754a54c8ec641d066521c2422e0fb12016ca9f51
Monza-xee <axehuyyy@gmail.com>
2026-04-15 07:55:11 +0700

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://vpebmpjgbntoqxxmetwn.supabase.co";
-const supabaseKey = "PASTE_API_KEY_LU";
+const supabaseKey = "sb_publishable_vBcsnDyLfO0bkvvegDsT7Q_ac8OuWQd";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
