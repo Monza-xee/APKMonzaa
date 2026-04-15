68964813fd12fd0b38dd951014825a7330646fca
Monza-xee <axehuyyy@gmail.com>
2026-04-15 08:03:35 +0700

    Delete lib/supabase.ts

--- lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://vpebmpjgbntoqxxmetwn.supabase.co";
-const supabaseKey = "sb_publishable_vBcsnDyLfO0bkvvegDsT7Q_ac8OuWQd";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
