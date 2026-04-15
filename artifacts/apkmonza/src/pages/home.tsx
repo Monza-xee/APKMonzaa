1b1048b3a753156ad2dde20203e0a7c3ea304dfc
Monza-xee <axehuyyy@gmail.com>
2026-04-15 14:44:50 +0700

    Update home.tsx

--- artifacts/apkmonza/src/pages/home.tsx
+++ artifacts/apkmonza/src/pages/home.tsx
@@ -40,7 +40,7 @@ export function Home() {
           <Input
             value={search}
             onChange={(e) => setSearch(e.target.value)}
-            placeholder="SEARCH CATALOG HAAy..."
+            placeholder="SEARCH CATALOG Htws..."
             className="pl-10 border-2 border-black rounded-none font-mono uppercase"
           />
         </div>
